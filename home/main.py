653c55e2864e6577cbc2532adae26364a192477c
Arthur Tofani <gramofone@gmail.com>
2020-09-01 12:05:09 -0300

    improved tokenization method

--- home/main.py
+++ home/main.py
@@ -4,11 +4,13 @@ from multiprocessing import Pool
 from itertools import chain, combinations, product
 from footprint.models import Project
 import footprint.evaluators as evaluators
+from itertools import combinations
 import os
 import logging
 import numpy as np
 import pandas as pd
 import db
+import csv
 
 #features
 import features
@@ -35,51 +37,69 @@ results_file = '/cache/results.txt'
 db_index_name = 'mirex_2020'
 
 
-tks = [['tk_i', 'tk_g' ,'tk_h', 'tk_c'], 'tk_a', 'tk_b', 'tk_c', 'tk_d', 'tk_e', 'tk_f', 'tk_g', 'tk_h']
-for tk in tks:
+valid_cols = ['chord_type', 'beat_type', 'melody_note', 'bass_note', 'harm_note', 'downbeat', 'strong_beat', 'weak_beat']
+candidate_cols = [[x] for x in valid_cols]
+candidate_cols += [list(x) for x in list(combinations(valid_cols, 2))]
+candidate_cols += [list(x) for x in list(combinations(valid_cols, 3))]
+candidate_cols += [list(x) for x in list(combinations(valid_cols, 4))]
 
-  # create new project with the default CSI evaluator
-  project = Project(cache_folder='/cache/project', cache_features=True, cache_tokens=False, cache_signal=True)
-
-  evaluator = evaluators.CSI(project)
-
-  # Initialize project's db client
-  db.connect_to_elasticsearch(project, db_index_name, True)
-  #project.client.set_scope(db_index_name, ['tk_b'], 'tokens_by_spaces')
-
-  project.client.set_scope(db_index_name, tk, 'tokens_by_spaces')
-
-  # Declare methods used to extract features
-  project.process_feature('feat', features.complete_extraction)
-
-  # Declare tokenization strategies to be used
-  project.tokenize('tk_a', tokenization.grouped_tokens(['beat_type', 'chord_type', 'harm_note', 'melody_note', 'bass_note']))
-  project.tokenize('tk_b', tokenization.grouped_tokens(['chord_type', 'harm_note', 'melody_note', 'bass_note']))
-  project.tokenize('tk_c', tokenization.grouped_tokens(['chord_type', 'harm_note', 'melody_note']))
-  project.tokenize('tk_d', tokenization.grouped_tokens(['beat_type', 'chord_type', 'harm_note', 'bass_note']))
-  project.tokenize('tk_e', tokenization.grouped_tokens(['beat_type', 'chord_type', 'melody_note', 'bass_note']))
-  project.tokenize('tk_f', tokenization.grouped_tokens(['beat_type', 'chord_type', 'melody_note']))
-  project.tokenize('tk_g', tokenization.grouped_tokens(['beat_type', 'chord_type', 'harm_note']))
-  project.tokenize('tk_h', tokenization.grouped_tokens(['beat_type', 'chord_type', 'bass_note']))
-  project.tokenize('tk_i', tokenization.grouped_tokens(['beat_type', 'chord_type', 'melody_note']))
-
-  evaluator.build(files_map.file.values)
-
-  evaluator.match(queries_map.file.values, amnt_results_per_query=10)
-
-  ev = evaluator.evaluate()
+def read_clique_map(filename):
+  f = open(filename, 'r', encoding='utf-8')
+  s = list(csv.reader(f, delimiter='\t'))
+  f.close()
+  return dict([[x[1], x[0]] for x in s])
 
 
-  import csv
-  def read_clique_map(filename):
-    f = open(filename, 'r', encoding='utf-8')
-    s = list(csv.reader(f, delimiter='\t'))
-    f.close()
-    return dict([[x[1], x[0]] for x in s])
-  clique_file = '/cache/clique_file.csv'
-  df_clique = files_map.append(queries_map)
-  df_clique['clique'] = df_clique.key.map(lambda x: x.split('/')[5])
-  df_clique[['clique', 'file']].to_csv(clique_file, index=False, header=False, sep='\t')
-  #import code; code.interact(local=dict(globals(), **locals()))
-  print(evaluator.results(read_clique_map(clique_file), ranking_size=10)[0].T)
+#import code; code.interact(local=dict(globals(), **locals()))
+results_df = None
+
+for idx, tk_cols in enumerate(candidate_cols):
+  for ngrams in [1, 2, 3, 4]:
+
+    # create new project with the default CSI evaluator
+    project = Project(cache_folder='/cache/project', cache_features=True, cache_tokens=False, cache_signal=True)
+
+    evaluator = evaluators.CSI(project)
+
+    # Initialize project's db client
+    db.connect_to_elasticsearch(project, db_index_name, True)
+    #project.client.set_scope(db_index_name, ['tk_b'], 'tokens_by_spaces')
+
+    token_code = 'tk_%s' % idx
+    project.client.set_scope(db_index_name, token_code, 'tokens_by_spaces')
+
+    # Declare methods used to extract features
+    project.process_feature('feat', features.complete_extraction)
+
+    # Declare tokenization strategies to be used
+    project.tokenize(token_code, tokenization.grouped_tokens(tk_cols, idx, ngrams_size=ngrams))
+
+    evaluator.build(files_map.file.values)
+
+    evaluator.match(queries_map.file.values, amnt_results_per_query=10)
+
+    ev = evaluator.evaluate()
+
+
+
+
+    clique_file = '/cache/clique_file.csv'
+    df_clique = files_map.append(queries_map)
+    df_clique['clique'] = df_clique.key.map(lambda x: x.split('/')[5])
+    df_clique[['clique', 'file']].to_csv(clique_file, index=False, header=False, sep='\t')
+    r = evaluator.results(read_clique_map(clique_file), ranking_size=10)[0]
+    r['cols_idx'] = idx
+    r['ngrams'] = ngrams
+    for col in valid_cols:
+      r['col_%s' % col] = 0
+    for col in tk_cols:
+      r['col_%s' % col] = 1
+    if results_df is None:
+      results_df = r
+    else:
+      results_df = results_df.append(r)
+    print(evaluator.results(read_clique_map(clique_file), ranking_size=10)[0].T)
+    #import code; code.interact(local=dict(globals(), **locals()))
+    results_df.to_csv('/cache/tokens_results.csv', index=False, header=True, sep='\t')
+
 
--- home/tokenization.py
+++ home/tokenization.py
@@ -1,15 +1,38 @@
 import pandas as pd
+import numpy as np
 
-def grouped_tokens(fields):
+def normalize_dataframe(df_orig):
+  df = df_orig.copy()
+  chord_type_values = ['maj', 'min', 'sus', 'aug', 'dim', 'x']
+  beat_type_values = ['d', 'u', 'b', 'm']
+  df['chord_type'] = df.chord_type.map(lambda x: chord_type_values.index(x))
+  df['beat_type'] = df.beat_type.map(lambda x: beat_type_values.index(x))
+  df['melody_note'] = df.melody_note+1
+  df['bass_note'] = df.bass_note+1
+  df['harm_note'] = df.harm_note+1
+  df['downbeat'] = df['strong_beat'] = df['weak_beat'] = 0
+  df.loc[df.beat_type==0, 'downbeat'] = 1
+  df.loc[df.beat_type.isin([0, 1, 2]), 'strong_beat'] = 1
+  df.loc[df.beat_type==3, 'weak_beat'] = 1
+  return df
+
+def ngrams(arr_tokens, n, tk_idx):
+  tk = str(tk_idx)
+  ngrams = [tk + '#' + ':'.join(x) for x in zip(*[arr_tokens[i:] for i in range(n)])]
+  return ngrams
+
+def compact_token(df, cols):
+  arr = np.array(df[cols])
+  return [''.join(map(lambda z: format(z, 'x'), x)) for x in arr]
+
+def grouped_tokens(fields, tk_idx, ngrams_size=2):
   flds = fields
+  idx = tk_idx
+  n = ngrams_size
   def _grouped_tokens(document):
-    d = document.features['feat'].copy()
-    d['res'] = ''
-    for c in flds:
-        d['res'] += (d[c].map(str) + ':')
-    tk1 = d.res.values
-    tk2 = [a + '::' + b for a, b in list(zip(tk1, tk1[1:]))]
-    tk3 = [a + '::' + b for a, b in list(zip(tk2, tk1[2:]))]
-    tokens = ' '.join(tk1) + ' ' +  ' '.join(tk2) + ' ' + ' '.join(tk3)
-    return tokens
+    d = normalize_dataframe(document.features['feat'])
+    compacted_tokens = compact_token(d, fields)
+    ngrammed_tokens =  ngrams(compacted_tokens, n, idx)
+    tk = ' '.join(ngrammed_tokens)
+    return tk
   return _grouped_tokens
